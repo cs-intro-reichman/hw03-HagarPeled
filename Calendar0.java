79b55f4df59ef61915a5993154ecc24736bf81ad
HagarPeled <hagar.peled@post.runi.ac.il>
2024-01-03 20:28:57 +0200

    Homework 3

--- Calendar0.java
+++ Calendar0.java
@@ -22,13 +22,22 @@ public class Calendar0 {
 
 	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
+		for (int month=1; month<=12; month++) {
+		int daysInMonth = nDaysInMonth(month, year);	
+	    System.out.println("Month " + month + " has " + daysInMonth + " days");
 	}
+}
 
 	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		return false;
+     boolean isLeapYear;
+     
+     if ((year% 400 ==0) || ((year % 4 == 0) && (year % 100 != 0))) {
+     	isLeapYear= true; 
+     } else {
+     	isLeapYear = false;
+     }
+		return isLeapYear;
 	}
 	 
 	// Returns the number of days in the given month and year.
@@ -36,8 +45,18 @@ public class Calendar0 {
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
 	public static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    switch (month) {
+        case 2: 
+            return isLeapYear(year) ? 29 : 28;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+        default:
+            return 31; 	
+     }
+  }
+
 }
 
